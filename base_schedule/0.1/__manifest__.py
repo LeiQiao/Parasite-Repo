40850ed8fe0c8f3b7cae9132d81c3723000f8ea4
lei.qiao <leiqiaotalk@hotmail.com>
2018-12-05 22:03:41 +0800

    2018.12.05, add plugin 'base_schedule'

---
+++ base_schedule/0.1/__manifest__.py
@@ -0,0 +1,16 @@
+# noinspection PyStatementEffect
+{
+    'name': 'base_schedule',
+    'summary': '',
+    'description': '可以在 manifest 中添加定时任务并将其绑定到某个任务对象',
+    
+    'author': '',
+    'website': '',
+    'source': {'git': 'https://github.com/LeiQiao/Parasite-Plugins.git', 'branch': 'master'},
+
+    'category': 'base',
+    'version': '0.1',
+
+    # any plugin necessary for this one to work correctly
+    'depends': ['base']
+}
