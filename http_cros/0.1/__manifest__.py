77a258c73ac810fd5af41e1b9ff49a9dea7e7b53
lei.qiao <lei.qiao@chinapnr>
2018-12-18 17:40:44 +0800

    2018.12.18 add http_cros plugin
    allow http allow origin

---
+++ http_cros/0.1/__manifest__.py
@@ -0,0 +1,15 @@
+# noinspection PyStatementEffect
+{
+    'name': 'http_cros',
+    'summary': '跨域允许',
+    'description': '',
+    'author': '',
+    'website': '',
+    'source': {'git': 'https://github.com/LeiQiao/Parasite-Plugins.git', 'branch': 'master'},
+
+    'category': '',
+    'version': '0.1',
+
+    # any plugin necessary for this one to work correctly
+    'depends': ['base']
+}
