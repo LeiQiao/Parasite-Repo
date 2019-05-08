bebfe16461bb3cab889aa5a24d46a1473bdae511
lei.qiao <lei.qiao@chinapnr>
2019-05-08 14:04:47 +0800

    2019.05.08 add simple_html plugin

---
+++ simple_html/0.1/__manifest__.py
@@ -0,0 +1,15 @@
+# noinspection PyStatementEffect
+{
+    'name': 'simple_html',
+    'summary': '简单的 HTML 页面模板',
+    'description': '简单的 HTML 页面模板，可以注册指定的 HTML 页面，也可以通过继承的方式改变页面的内容',
+    'author': '',
+    'website': '',
+    'source': {'git': 'https://github.com/LeiQiao/Parasite-Plugins.git', 'branch': 'master'},
+
+    'category': 'html',
+    'version': '0.1',
+
+    # any plugin necessary for this one to work correctly
+    'depends': ['base']
+}
