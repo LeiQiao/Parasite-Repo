9758b15b6c92de42a5a3f73d5b2579534670a6af
lei.qiao <lei.qiao@chinapnr>
2019-05-31 14:51:31 +0800

    添加 i18n 插件

---
+++ i18n/0.1/__manifest__.py
@@ -0,0 +1,15 @@
+# noinspection PyStatementEffect
+{
+    'name': 'i18n',
+    'summary': '字符串国际化',
+    'description': '字符串国际化插件，定义提示语、文字等国际化标准，允许其它插件覆盖扩扩展',
+    'author': '',
+    'website': '',
+    'source': {'git': 'https://github.com/LeiQiao/Parasite-Plugins.git', 'branch': 'master'},
+
+    'category': '',
+    'version': '0.1',
+
+    # any plugin necessary for this one to work correctly
+    'depends': ['base']
+}
