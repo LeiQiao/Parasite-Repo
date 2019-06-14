5c5b63de282a69ab299580b20caa29d2c9894260
lei.qiao <lei.qiao@chinapnr>
2019-06-14 17:09:02 +0800

    增加 record_api 插件

---
+++ record_api/0.1/__manifest__.py
@@ -0,0 +1,15 @@
+# noinspection PyStatementEffect
+{
+    'name': 'record_api',
+    'summary': '',
+    'description': '提供简单的增删改查的接口来操作数据库记录',
+    'author': '',
+    'website': '',
+    'source': {'git': 'https://github.com/LeiQiao/Parasite-Plugins.git', 'branch': 'master'},
+
+    'category': '',
+    'version': '0.1',
+
+    # any plugin necessary for this one to work correctly
+    'depends': ['base', 'base_api_wrapper', 'base_db_manager', 'i18n']
+}
