cedd59cd874102269d8516c721dda00fc0d01bbc
lei.qiao <lei.qiao@chinapnr>
2019-06-20 16:13:43 +0800

    添加 jfile sdk

---
+++ jarvis_file_client/0.1/__manifest__.py
@@ -0,0 +1,15 @@
+# noinspection PyStatementEffect
+{
+    'name': 'jarvis_file_client',
+    'summary': 'Jarvis File 客户端 SDK',
+    'description': '',
+    'author': '',
+    'website': '',
+    'source': {'git': 'https://github.com/LeiQiao/Parasite-Plugins.git', 'branch': 'master'},
+
+    'category': '',
+    'version': '0.1',
+
+    # any plugin necessary for this one to work correctly
+    'depends': ['base']
+}
